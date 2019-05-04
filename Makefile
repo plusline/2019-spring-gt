0c6669653f51a33e413ce54cf1fc6d476ec20932
plusline <jason12399@gmail.com>
2019-05-04 14:58:45 +0800

    some prace and add optional function

--- Makefile
+++ Makefile
@@ -3,12 +3,16 @@ OBJS:=fakemn
 LIBS:=fake-mininet/lib
 CXXFLAGS:= -std=c++11
 
+
 all: $(EXEC)
 
 # executable (using static library)
 # need libfakemn.a (compile from the Makefile of project root)
 $(EXEC): %.out: %.cc
 	g++ -o $@ $< -I$(LIBS) -g3 -L. -l$(OBJS) $(CXXFLAGS) -no-pie
+
+plot: plot.dot
+	dot -Tsvg plot.dot > picture.svg
 
 # reset & clean
 clean: 
--- main.cc
+++ main.cc
@@ -16,6 +16,19 @@ int main(int argc, char** argv){
     /* start your program */
     nm->interpret("graph.txt");
     nm->print_all_e();
+    nm->print_all_v();
+
+    //find path
+    Path *path;
+    path=new Path();
+    path->append(nm->elist);
+    std::vector<std::vector<Edge *>> avail_paths = path->find_paths(std::string("c"),std::string("b"));
+    path->debug();
+    for(int x=0;x<path->paths.at(0).size();x++)
+    {
+        cout<<path->paths.at(0).at(x)->head->name<<"->"<<path->paths.at(0).at(x)->tail->name<<" ";
+    }
+
 
     // using gplot to export a  plot file
     Gplot *gp = new Gplot();
